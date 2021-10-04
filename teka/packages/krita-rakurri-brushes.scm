fbbfbffb3e7cd0ef00dd08cd4cab9eb8de7373d0
tekakutli <tekakutli@outlook.com>
2021-10-04 12:15:47 -0500

    more

--- teka/packages/krita-rakurri-brushes.scm
+++ teka/packages/krita-rakurri-brushes.scm
@@ -7,19 +7,20 @@
   #:use-module (gnu packages)
   #:use-module (srfi srfi-1))
 
-(package
-  (name "rakurri-brush-set-for-krita")
-  (version "1.1")
-  (source (origin
+(define-public rakurri-brush-set-for-krita
+  (package
+   (name "rakurri-brush-set-for-krita")
+   (version "1.1")
+   (source (origin
             (method url-fetch)
             (uri (string-append "mirror://gnu/hello/hello-" version
                                 ".tar.gz"))
             (sha256
              (base32
               "1csay05vv477012hz9z2y1662axgmhbbw8kl7i9323bm7mfhc2m4"))))
-  (build-system gnu-build-system)
-  (synopsis "Awesome brushes developed by Rakurri for Krita 5")
-  (description
-   "better to see them yourself")
-  (home-page "https://github.com/Rakurri/rakurri-brush-set-for-krita")
-  (license gpl3+))
+   (build-system gnu-build-system)
+   (synopsis "Awesome brushes developed by Rakurri for Krita 5")
+   (description
+    "better to see them yourself")
+   (home-page "https://github.com/Rakurri/rakurri-brush-set-for-krita")
+   (license gpl3+)))
