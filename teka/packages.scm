01c57534e0ed1c7e5f5a11c5bf270b77f18e8af3
tekakutli <tekakutli@outlook.com>
2021-10-04 12:42:07 -0500

    more

--- teka/packages.scm
+++ teka/packages.scm
@@ -23,24 +23,24 @@
             search-patches
             %patch-path))
 
-(define (search-patch file-name)
-  "Search the patch FILE-NAME.  Raise an error if not found."
-  (or (search-path (%patch-path) file-name)
-      (raise (formatted-message (G_ "~a: patch not found")
-                                file-name))))
+;; (define (search-patch file-name)
+;;   "Search the patch FILE-NAME.  Raise an error if not found."
+;;   (or (search-path (%patch-path) file-name)
+;;       (raise (formatted-message (G_ "~a: patch not found")
+;;                                 file-name))))
 
-(define-syntax-rule (search-patches file-name ...)
-  "Return the list of absolute file names corresponding to each
-FILE-NAME found in %PATCH-PATH."
-  (list (search-patch file-name) ...))
+;; (define-syntax-rule (search-patches file-name ...)
+;;   "Return the list of absolute file names corresponding to each
+;; FILE-NAME found in %PATCH-PATH."
+;;   (list (search-patch file-name) ...))
 
-(define %channel-root
-  (find (lambda (path)
-          (file-exists? (string-append path "/teka/packages.scm")))
-        %load-path))
+;; (define %channel-root
+;;   (find (lambda (path)
+;;           (file-exists? (string-append path "/teka/packages.scm")))
+;;         %load-path))
 
-(define %patch-path
-  (make-parameter
-   (cons
-    (string-append %channel-root "/teka/packages/patches")
-    (gnu:%patch-path))))
+;; (define %patch-path
+;;   (make-parameter
+;;    (cons
+;;     (string-append %channel-root "/teka/packages/patches")
+;;     (gnu:%patch-path))))
