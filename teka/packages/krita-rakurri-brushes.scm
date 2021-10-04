11c6c3df80dfb75f2fe5eaeff0670639ce10e17a
tekakutli <tekakutli@outlook.com>
2021-10-04 12:51:08 -0500

    more

--- teka/packages/krita-rakurri-brushes.scm
+++ teka/packages/krita-rakurri-brushes.scm
@@ -4,6 +4,7 @@
   #:use-module (guix memoization)
   #:use-module (guix utils)
   #:use-module (guix build-system gnu)
+  #:use-module (guix git-download)
   #:use-module (gnu packages)
   #:use-module (guix licenses)
   #:use-module (srfi srfi-1))
@@ -14,8 +15,8 @@
    (version "1.1")
    (source (origin
             (method git-fetch)
-            (uri (string-append "mirror://gnu/hello/hello-" version
-                                ".tar.gz"))
+            (git-reference (url "https://github.com/Rakurri/rakurri-brush-set-for-krita")
+            (commit "a4d915686ad8d7fd565c4d095329a562a6655662"))
             (sha256
              (base32
               "1csay05vv477012hz9z2y1662axgmhbbw8kl7i9323bm7mfhc2m4"))))
