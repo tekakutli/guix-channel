18d55b4a552141b2b4e8cb50d023f7b95363e1b1
tekakutli <tekakutli@outlook.com>
2021-10-04 11:17:12 -0500

    more

--- teka/packages/krita-rakurri-brushes.scm
+++ teka/packages/krita-rakurri-brushes.scm
@@ -4,15 +4,15 @@
              (guix licenses))
 
 (package
-  (name "my-hello")
-  (version "2.10")
+  (name "rakurri-brush-set-for-krita")
+  (version "1.1")
   (source (origin
             (method url-fetch)
             (uri (string-append "mirror://gnu/hello/hello-" version
                                 ".tar.gz"))
             (sha256
              (base32
-              "0ssi1wpaf7plaswqqjwigppsg5fyh99vdlb9kzl7c9lng89ndq1i"))))
+              "1csay05vv477012hz9z2y1662axgmhbbw8kl7i9323bm7mfhc2m4"))))
   (build-system gnu-build-system)
   (synopsis "Awesome brushes developed by Rakurri for Krita 5")
   (description
